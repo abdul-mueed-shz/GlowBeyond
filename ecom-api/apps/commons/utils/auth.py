2f7a5885920481c7707804ddada5808e11a07a61
Abdul-Mueed-Shahbaz <abdulmueedshahbaz@gmail.com>
2022-12-28 21:30:26 +0500

    "Project completion commit"

--- ecom-api/apps/commons/utils/auth.py
+++ ecom-api/apps/commons/utils/auth.py
@@ -9,7 +9,7 @@ class JwtAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get('auth_token') or request.META.get('HTTP_AUTHTOKEN')
         if not token:
-            raise AuthenticationFailed("Unauthenticated")
+            raise AuthenticationFailed("Unauthenticated!")
 
         try:
             payload = jwt.decode(token, 'ragnaroks_auth_application', algorithms=['HS256'])
